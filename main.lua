02d39ed370e2abfe4b370bb07b412474f1a36c38
robodmanx <43498235+robodmanx@users.noreply.github.com>
2022-08-04 03:48:32 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,20 @@
-s
+game.Players.LocalPlayer.CharacterAdded:Connect(function(chr)
+    wait(1)
+    for i,v in pairs(workspace:GetChildren()) do
+        if v.ClassName == 'Model' then
+            if v:FindFirstChild('BagTouchScript') then
+                local type = v.Rarity.Value
+                if table.find({'Ultimate','Resplendent'}, type) then
+                    warn('ITS HAPPENING')
+                    break
+                end
+            end
+        end
+    end
+    game.Players.LocalPlayer.OnTeleport:Connect(function(s)
+        if s == Enum.TeleportState.Started then
+            syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/robodmanx/auto-bag-farm/main/main.lua'))()")
+        end
+    end)
+    game:GetService('TeleportService'):Teleport(game.PlaceId, game.Players.LocalPlayer)
+end)
